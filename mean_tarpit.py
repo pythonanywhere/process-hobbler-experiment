bffb5599844c62bfa455c2bb6dc505c51bbb0c0b
Harry <hjwp2@cantab.net>
2015-10-02 12:21:42 +0000

    recursively stop and continue child processes. by: Harry

--- mean_tarpit.py
+++ mean_tarpit.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3.4
 import asyncio
+from concurrent.futures import ThreadPoolExecutor
 import os
+import psutil
 import signal
 import sys
+
 
 
 @asyncio.coroutine
@@ -12,13 +15,22 @@ def get_pids(tarpit_cgroup_dir):
 
 
 @asyncio.coroutine
-def hobble_process(pid):
+def hobble_process(pid, loop):
     print('hobbling pid', pid)
     pid = int(pid)
+    process = psutil.Process(pid)
     while True:
         try:
             os.kill(pid, signal.SIGSTOP)
+            children = yield from loop.run_in_executor(
+                loop.threadpool,
+                lambda: process.children(recursive=True)
+            )
+            for child in children:
+                os.kill(child.pid, signal.SIGSTOP)
             yield from asyncio.sleep(0.2)
+            for child in reversed(children):
+                os.kill(child.pid, signal.SIGCONT)
             os.kill(pid, signal.SIGCONT)
             yield from asyncio.sleep(0.01)
 
@@ -36,7 +48,7 @@ def hobble_current_processes(loop, already_hobbled, tarpit_cgroup_dir):
             continue
         already_hobbled.add(pid)
         loop.create_task(
-            hobble_process(pid)
+            hobble_process(pid, loop)
         )
     print('now hobbling {} processes'.format(len(already_hobbled)))
 
@@ -45,13 +57,16 @@ def hobble_current_processes(loop, already_hobbled, tarpit_cgroup_dir):
 def hobble_processes_forever(loop, tarpit_cgroup_dir):
     already_hobbled = set()
     while True:
-        print('hobbling current processes')
+        print('hobbling current processes', flush=True)
         yield from hobble_current_processes(loop, already_hobbled, tarpit_cgroup_dir)
         yield from asyncio.sleep(2)
 
 
 def main(tarpit_cgroup_dir):
     loop = asyncio.get_event_loop()
+    if not hasattr(loop, 'create_task'):  # was added in 3.4.2
+        loop.create_task = asyncio.async
+    loop.threadpool = ThreadPoolExecutor(max_workers=20)
     loop.create_task(
         hobble_processes_forever(loop, tarpit_cgroup_dir)
     )
